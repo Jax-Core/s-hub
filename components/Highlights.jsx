30e055d1047c062b5a90be5dc75af5f0154f8b3f
vo1x <97158976+vo1x@users.noreply.github.com>
2022-07-12 19:14:07 +0545

    Update Highlights.jsx

--- components/Highlights.jsx
+++ components/Highlights.jsx
@@ -6,13 +6,13 @@ import Send from '../public/assets/Send.svg';
 
 const Highlights = () => {
   return (
-    <div id='skills' className='w-full p-2'>
+    <div className='w-full p-2'>
       <div className='max-w-screen-2xl mx-auto flex flex-col justify-center h-full'>
 
         <h2 className='py-6 text-6xl font-bold pb-20'>Main Highlights</h2>
 
-      <div class="flex flex-wrap -m-2">
-        <div class="p-4 md:w-1/3">
+      <div className="flex flex-wrap -m-2">
+        <div className="p-4 md:w-1/3">
           <div className="flex rounded-3xl h-full shadow-[0px_4px_20px_3px_#D8DEE9] p-8 flex-col">
           <div className='m-auto'>
                 <Image src={Dashboard} width='96px' height='96px' alt='/' />
