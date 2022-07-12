5ee9181e16587297e68a915a6f85035fbbafcd9f
vo1x <97158976+vo1x@users.noreply.github.com>
2022-07-12 19:05:23 +0545

    Fixed the cards and added responsiveness

--- components/Highlights.jsx
+++ components/Highlights.jsx
@@ -10,42 +10,64 @@ const Skills = () => {
       <div className='max-w-screen-2xl mx-auto flex flex-col justify-center h-full'>
 
         <h2 className='py-6 text-6xl font-bold pb-20'>Main Highlights</h2>
-        <div className='grid grid-cols-3 gap-8'>
-          
-        <div className='p-6 shadow-lg rounded-xl'>
-            <div className='grid grid-rows-2 justify-center items-center'>
-              <div className='m-auto'>
+
+      <div class="flex flex-wrap -m-2">
+        <div class="p-4 md:w-1/3">
+          <div className="flex rounded-3xl h-full shadow-[0px_4px_20px_3px_#D8DEE9] p-8 flex-col">
+          <div className='m-auto'>
                 <Image src={Dashboard} width='96px' height='96px' alt='/' />
               </div>
-              <div className='flex flex-col items-center justify-center'>
-                <span className='text-3xl font-semibold py-2'>Large variety of setups</span>
-                <span className='text-xl'>Short description goes here</span>
-              </div>
+            <div class="flex items-center justify-center mb-3">
+              <h2 class="text-3xl title-font font-semibold">Large variety of setups</h2>
             </div>
-          </div>
-          <div className='p-6 shadow-lg rounded-xl'>
-            <div className='grid grid-rows-2 justify-center items-center'>
-              <div className='m-auto'>
-                <Image src={Download} width='96px' height='96px' alt='/' />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <span className='text-3xl font-semibold py-2'>Large variety of setups</span>
-                <span className='text-xl'>Short description goes here.</span>
-              </div>
-            </div>
-          </div>
-          <div className='p-6 shadow-lg rounded-xl '>
-            <div className='grid grid-rows-2 justify-center items-center'>
-              <div className='m-auto'>
-                <Image src={Send} width='96px' height='96px' alt='/' />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <span className='text-3xl font-semibold py-2'>Large variety of setups</span>
-                <span className='text-xl'>Short description goes here</span>
-              </div>
+            <div class="flex-grow">
+              <p class="leading-relaxed text-base">
+                We're no strangers to love
+                You know the rules and so do I (do I)
+                A full commitment's what I'm thinking of
+                You wouldn't get this from any other guy</p>
             </div>
           </div>
         </div>
+
+        <div class="p-4 md:w-1/3">
+          <div className="flex rounded-3xl h-full shadow-[0px_8px_20px_3px_#D8DEE9] p-8 flex-col">
+          <div className='m-auto'>
+                <Image src={Download} width='96px' height='96px' alt='/' />
+              </div>
+            <div class="flex items-center justify-center mb-3">
+              <h2 class="text-3xl title-font font-semibold">Simple Instllation</h2>
+            </div>
+            <div class="flex-grow">
+              <p class="leading-relaxed text-base">
+              I just wanna tell you how I'm feeling
+              Gotta make you understand
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-4 md:w-1/3">
+          <div className="flex rounded-3xl h-full shadow-[0px_8px_20px_3px_#D8DEE9] p-8 flex-col">
+          <div className='m-auto'>
+                <Image src={Send} width='96px' height='96px' alt='/' />
+              </div>
+            <div class="flex items-center justify-center mb-3">
+              <h2 class="text-3xl title-font font-semibold">Open Submissions</h2>
+            </div>
+            <div class="flex-grow">
+              <p class="leading-relaxed text-base">
+                Never gonna give you up
+                Never gonna let you down
+                Never gonna run around and desert you
+                Never gonna make you cry
+                Never gonna say goodbye
+                Never gonna tell a lie and hurt you
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     </div>
   );
