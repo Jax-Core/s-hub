3a21de77eace9eac9a5e39843008bb8dd4d4972d
vo1x <97158976+vo1x@users.noreply.github.com>
2022-07-12 17:44:32 +0545

    delete unused files

--- pages/core.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const core = () => {
-  return (
-    <div>work in progress</div>
-  );
-};
-
-export default core;
--- pages/faq.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const faq = () => {
-  return (
-    <div>work in progress</div>
-  );
-};
-
-export default faq;
